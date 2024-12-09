bc524ea983fb4a361a3222b4f5b267aa51564b23
mahtabsiddique <mahtabsiddique@localhost>
2024-12-09 07:47:06 +0530

    First TC- Order by ID.

--- Object Repository/Swagger Petstore/getOrderById.rs
+++ Object Repository/Swagger Petstore/getOrderById.rs
@@ -8,7 +8,7 @@
    <smartLocatorEnabled>false</smartLocatorEnabled>
    <useRalativeImagePath>false</useRalativeImagePath>
    <autoUpdateContent>true</autoUpdateContent>
-   <connectionTimeout>-1</connectionTimeout>
+   <connectionTimeout>0</connectionTimeout>
    <followRedirects>false</followRedirects>
    <httpBody></httpBody>
    <httpBodyContent></httpBodyContent>
@@ -26,11 +26,11 @@
       <matchCondition>equals</matchCondition>
       <name>Content-type</name>
       <type>Main</type>
-      <value>application/xml</value>
+      <value>application/json</value>
       <webElementGuid>6e50eaa4-302c-432b-b351-877e2a98d406</webElementGuid>
    </httpHeaderProperties>
    <katalonVersion>10.0.0</katalonVersion>
-   <maxResponseSize>-1</maxResponseSize>
+   <maxResponseSize>0</maxResponseSize>
    <migratedVersion>5.4.1</migratedVersion>
    <path>/store/order/{orderId}</path>
    <restRequestMethod>GET</restRequestMethod>
@@ -41,14 +41,27 @@
    <soapRequestMethod></soapRequestMethod>
    <soapServiceEndpoint></soapServiceEndpoint>
    <soapServiceFunction></soapServiceFunction>
-   <socketTimeout>-1</socketTimeout>
+   <socketTimeout>0</socketTimeout>
    <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
    <variables>
-      <defaultValue>''</defaultValue>
+      <defaultValue>5</defaultValue>
       <description></description>
       <id>7e8f21ca-9073-405b-81af-367cde6c170c</id>
       <masked>false</masked>
       <name>orderId</name>
    </variables>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
    <wsdlAddress></wsdlAddress>
 </WebServiceRequestEntity>
